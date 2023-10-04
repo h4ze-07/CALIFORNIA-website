2163f2b6216337d3ef9fb9608f5426caac38c50b
ZRJul <mzr.juli@gmail.com>
2023-10-04 19:06:44 +0300

    Jul-secondSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,13 @@ import Products from '../components/Products'
 import BestSection from '../components/BestSection'
 import Ideas from '../components/Ideas'
 import Else from '../components/Else'
-
+import ShopSection from "../components/ShopSection.jsx";
 
 const Home = () => {
   return (
     <>
         <Banner/>
+        <ShopSection/>
         <Products />
         <BestSection />
         <Ideas />
