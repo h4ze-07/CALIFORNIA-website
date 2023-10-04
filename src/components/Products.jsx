1e44e7f8de9933aed2c4ae5bc9ca1d28b14c033e
Viktoriia-Barvynska <vikag7593@gmail.com>
2023-10-04 10:30:28 +0300

    add sass to project

---
+++ src/components/Products.jsx
@@ -0,0 +1,44 @@
+import items1 from '../images/items1.jpg'
+import items2 from '../images/items2.jpg'
+import items3 from '../images/items3.jpg'
+import items4 from '../images/items4.jpg'
+
+const productsItem = [
+  {
+    id: 1,
+    title: 'MacBook Pro 13',
+    description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet',
+    img: items1,
+    price: 1200
+  },
+  {
+    id: 2,
+    title: 'Smart Galaxy Watch 3',
+    description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet',
+    img: items2,
+    price: 199
+  },
+  {
+    id: 3,
+    title: 'MacBook Air M1',
+    description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet',
+    img: items3,
+    price: 1009
+  },
+  {
+    id: 4,
+    title: 'iPad',
+    description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet',
+    img: items4,
+    price: 610
+  }
+]
+
+
+const Products = () => {
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
