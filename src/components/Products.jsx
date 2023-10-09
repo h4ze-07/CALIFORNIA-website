52e674a427ac9b7048218d0c7f8edafad4989135
ZRJul <mzr.juli@gmail.com>
2023-10-09 20:52:23 +0300

    styles

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -24,57 +24,52 @@ const Products = ({swiperProducts, addToCart}) => {
 //   };
 // });
 
-  const handleCartButtonClick = (product) => {
-    addToCart(product)
-  }
+    const handleCartButtonClick = (product) => {
+        addToCart(product)
+    }
 
-  return (
-    <>
-    <section className='items'>
-      <div className='container'>
-  
-         <div className='items_text text_centre'>
-         <h3>Save on our most selled items.</h3>
-         <p>Our new Limited Edition Winter Design BESPOKE 4-Door Flex™</p>
-         </div>
-        <Swiper
-         slidesPerView={4}
-         spaceBetween={30}
-         freeMode={true}
-         pagination={{
-           clickable: true,
-         }}
-         modules={[FreeMode, Pagination]}
-         className="mySwiper"
-         >
-         <div className='items_box d-flex between'>
-           <div className='box d-flex between'>
-            {swiperProducts.length === 0 && <p>No any products</p>} 
-                {swiperProducts.length > 0 && swiperProducts.map((product, id)=>( 
-               <SwiperSlide key={id}>
-               <article> 
-                  <img src={product.img} alt="" /> 
-                <a href="#">{product.name}</a> 
-                 <p>{product.description}</p> 
-              <h4> ${product.price}</h4> 
-              <button onClick={() => handleCartButtonClick(product)}>
-                Buy
-              </button>
-              <button> <BsSuitHeart/> </button> 
-              <button >
-                 
-              <Link to={`/product/${product.id}`}>More info</Link>
-                
-                </button>
-               </article> 
-               </SwiperSlide>
-             ))} 
-          </div> 
-        </div> 
-        </Swiper>
-      </div> 
-    </section>
- </> )
+    return (
+        <>
+            <section className='items'>
+                <div className='container'>
+
+                    <div className='items_text text_centre'>
+                        <h3>Save on our most selled items.</h3>
+                        <p>Our new Limited Edition Winter Design BESPOKE 4-Door Flex™</p>
+                    </div>
+                    <Swiper
+                        slidesPerView={4}
+                        spaceBetween={30}
+                        freeMode={true}
+                        autoplay={{delay: 3000}}
+                        loop={true}
+                        modules={[FreeMode, Autoplay]}
+                        className="mySwiper"
+                    >
+                        <div className='items_box d-flex between'>
+                            {swiperProducts.length === 0 && <p>No any products</p>}
+                            {swiperProducts.length > 0 && swiperProducts.map((product, id) => (
+                                <SwiperSlide key={id}>
+                                    <article>
+                                        <img src={product.img} alt=""/>
+                                        <h4>{product.name}</h4>
+                                        <p> ${product.price}</p>
+                                        <button onClick={() => handleCartButtonClick(product)}>
+                                            Edd to Cart
+                                        </button>
+                                        <button><BsSuitHeart/></button>
+                                        <button>
+                                            <Link to={`/product/${product.id}`}>More info</Link>
+                                        </button>
+                                    </article>
+                                </SwiperSlide>
+                            ))}
+
+                        </div>
+                    </Swiper>
+                </div>
+            </section>
+        </>)
 }
 
 
