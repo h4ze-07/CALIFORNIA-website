f13a0593b426073c2c4846437a3f967d3d5ae138
ZRJul <mzr.juli@gmail.com>
2023-10-09 19:50:32 +0300

    Jul-style_products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,14 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Link } from 'react-router-dom';
-import { BsSuitHeart } from "react-icons/bs";
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Autoplay} from "swiper/modules";
+import {Link} from 'react-router-dom';
+import {BsSuitHeart} from "react-icons/bs";
+import {FreeMode} from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
-
 import '../scss/products.scss'
 
-import { FreeMode, Pagination } from 'swiper/modules';
 
 const Products = ({swiperProducts}) => {
 
@@ -24,53 +24,46 @@ const Products = ({swiperProducts}) => {
 //   };
 // });
 
-  return (
-    <>
-    <section className='items'>
-      <div className='container'>
-  
-         <div className='items_text text_centre'>
-         <h3>Save on our most selled items.</h3>
-         <p>Our new Limited Edition Winter Design BESPOKE 4-Door Flex™</p>
-         </div>
-        <Swiper
-         slidesPerView={4}
-         spaceBetween={30}
-         freeMode={true}
-         pagination={{
-           clickable: true,
-         }}
-         modules={[FreeMode, Pagination]}
-         className="mySwiper"
-         >
-         <div className='items_box d-flex between'>
-           <div className='box d-flex between'>
-            {swiperProducts.length === 0 && <p>No any products</p>} 
-                {swiperProducts.length > 0 && swiperProducts.map((product, id)=>( 
-               <SwiperSlide key={id}>
-               <article> 
-                  <img src={product.img} alt="" /> 
-                <a href="#">{product.name}</a> 
-                 <p>{product.description}</p> 
-              <h4> ${product.price}</h4> 
-              <button>By</button>
-              <button> <BsSuitHeart/> </button> 
-              <button >
-                 
-              <Link to={`/product/${product.id}`}>More info</Link>
-                
-                </button>
-               </article> 
-               </SwiperSlide>
-             ))} 
-          </div> 
-        </div> 
-        </Swiper>
-      </div> 
-    </section>
- </> )
-}
+    return (
+        <>
+            <section className='items'>
+                <div className='container'>
 
+                    <div className='items_text text_centre'>
+                        <h3>Save on our most selled items.</h3>
+                        <p>Our new Limited Edition Winter Design BESPOKE 4-Door Flex™</p>
+                    </div>
+                    <Swiper
+                        slidesPerView={4}
+                        spaceBetween={30}
+                        freeMode={true}
+                        autoplay={{delay: 3000}}
+                        loop={true}
+                        modules={[FreeMode, Autoplay]}
+                        className="mySwiper"
+                    >
+                        <div className='items_box d-flex between'>
+                            {swiperProducts.length === 0 && <p>No any products</p>}
+                            {swiperProducts.length > 0 && swiperProducts.map((product, id) => (
+                                <SwiperSlide key={id}>
+                                    <article>
+                                        <img src={product.img} alt=""/>
+                                        <h4>{product.name}</h4>
+                                        <p> $ {product.price}</p>
+                                        <button>Edd to Cart</button>
+                                        <button><BsSuitHeart/></button>
+                                        <button>
+                                            <Link to={`/product/${product.id}`}>More info</Link>
+                                        </button>
+                                    </article>
+                                </SwiperSlide>
+                            ))}
+                        </div>
+                    </Swiper>
+                </div>
+            </section>
+        </>)
+}
 
 
 export default Products
